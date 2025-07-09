3e72936d31eb20eb96244b87463a2bb79c374228
manuelarte <manueldoncelmartos@gmail.com>
2025-07-10 00:04:27 +0200

    dev

---
+++ src/components/Player.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="player"
+    :class="[{ 'has-name': name }]"
+    :style="playerStyles"
+  >
+    <div class="number">{{ number }}</div>
+    <div v-if="name" class="name">{{ name }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    name: { type: String, default: 'Player' },
+    number: { type: Number, default: 10 },
+    shape: { type: String, default: 'circle' }, // 'circle' or 'shirt'
+  })
+</script>
+
+<style scoped>
+.player {
+  position: relative;
+  width: 50px;
+  height:50px;
+  background-color: #007bff;
+  color: white;
+  text-align: center;
+  cursor: default;
+  user-select: none;
+  border-radius: 50%;
+}
+
+.number {
+  font-size: 20px;
+  padding-top: 20%;
+  font-weight: bold;
+}
+
+.name {
+  font-weight: bold;
+  font-size: 10px;
+}
+</style>
