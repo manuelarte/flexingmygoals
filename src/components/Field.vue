378b864f563bf28ff5f54e45164d6e8c66e92c78
manuelarte <manueldoncelmartos@gmail.com>
2025-07-11 14:30:59 +0200

    using for function

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -9,6 +9,8 @@
 </script>
 
 <style scoped lang="sass">
+@use 'sass:list'
+
 $grass_color_primary: #3E7B3E
 $grass_color_secondary: #4A934A
 
@@ -18,28 +20,13 @@ $grass_color_secondary: #4A934A
   max-height: 400px
 
 .grass-gradient
-  background: linear-gradient(
-    0deg,
-$grass_color_primary 0%,
-$grass_color_primary 10%,
-$grass_color_secondary 10%,
-$grass_color_secondary 20%,
-$grass_color_primary 20%,
-$grass_color_primary 30%,
-$grass_color_secondary 30%,
-$grass_color_secondary 40%,
-$grass_color_primary 40%,
-$grass_color_primary 50%,
-$grass_color_secondary 50%,
-$grass_color_secondary 60%,
-$grass_color_primary 60%,
-$grass_color_primary 70%,
-$grass_color_secondary 70%,
-$grass_color_secondary 80%,
-$grass_color_primary 80%,
-$grass_color_primary 90%,
-$grass_color_secondary 90%,
-$grass_color_secondary 100%,
-)
+  $stops: ()
+  @for $i from 0 through 9
+    $start: $i * 10%
+    $end: ($i + 1) * 10%
+    $color: if($i % 2 == 0, $grass_color_primary, $grass_color_secondary)
+    $stops: list.append($stops, $color $start, comma)
+    $stops: list.append($stops, $color $end, comma)
+  background: linear-gradient(0deg, $stops)
 
 </style>
