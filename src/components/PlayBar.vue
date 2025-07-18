81afe9c97b677b965aaecd475b414a72df6e1f2c
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-07-18 14:18:46 +0200

    playbar skeleton

--- src/components/PlayBar.vue
+++ src/components/PlayBar.vue
@@ -1,16 +1,17 @@
 <template>
   <v-container>
-    <v-row no-gutters>
-      <v-col cols="auto">
-        <v-btn
-          class="ma-0"
-          :icon="getPlayBarIcon()"
-        />
-      </v-col>
-      <v-col cols="11">
-        <v-slider class="slider" />
-      </v-col>
-    </v-row>
+    <v-slider
+      v-model="timeValue"
+      append-icon="mdi-replay"
+      class="slider"
+      max="1"
+      min="0"
+      :prepend-icon="getPlayBarIcon()"
+      thumb-label="always"
+      @change="$emit('time-changed', $event)"
+      @click:prepend="$emit('toggle-play', !isPlaying)"
+      @update:model-value="timeChanged"
+    />
   </v-container>
 </template>
 
@@ -21,11 +22,33 @@
       type: Boolean,
       required: true,
     },
+    time: {
+      type: Number,
+      required: true,
+      validator (value: number, _: any): boolean {
+        return value >= 0 && value <= 100
+      },
+    },
   })
-  const iconMap = { true: 'mdi-pause', false: 'mdi-play' }
+
+  const emits = defineEmits(
+    {
+      'toggle-play': (_: boolean) => true,
+      'time-changed': (newTime: number) => {
+        return newTime >= 0 && newTime <= 100
+      },
+    },
+  )
+
+  const timeValue = ref(props.time)
 
   const getPlayBarIcon = (): string => {
     return props.isPlaying ? 'mdi-pause' : 'mdi-play'
+  }
+
+  const timeChanged = (): void => {
+    console.log('time changed')
+    emits('time-changed', timeValue.value)
   }
 </script>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <PlayBar :is-playing="false" />
+  <PlayBar
+    :is-playing="isPlaying"
+    :time="time"
+    @time-changed="time = $event"
+    @toggle-play="onTogglePlay"
+  />
   <Board />
 </template>
 
 <script lang="ts" setup>
-  //
+  const isPlaying = ref(false)
+  const time = ref(0.3)
+
+  const onTogglePlay = (newValue: boolean) => {
+    isPlaying.value = newValue
+  }
 </script>
