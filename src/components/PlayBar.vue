361825600c1622b61ce39176a6a1464125394ab7
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-07-18 09:50:38 +0200

    playbar skeleton

---
+++ src/components/PlayBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="auto">
+        <v-btn
+          class="ma-0"
+          :icon="getPlayBarIcon()"
+        />
+      </v-col>
+      <v-col cols="11">
+        <v-slider class="slider" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+  const props = defineProps({
+    isPlaying: {
+      type: Boolean,
+      required: true,
+    },
+  })
+  const iconMap = { true: 'mdi-pause', false: 'mdi-play' }
+
+  const getPlayBarIcon = (): string => {
+    return props.isPlaying ? 'mdi-pause' : 'mdi-play'
+  }
+</script>
+
+<style scoped lang="sass">
+.slider
+  vertical-align: middle
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <PlayBar :is-playing="false" />
   <Board />
 </template>
 
