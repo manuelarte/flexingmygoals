a3e60c7badbc384b7b909aaf935a247bcd3c856d
manuelarte <manueldoncelmartos@gmail.com>
2025-10-23 21:14:05 +0200

    adding more players to check

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,8 +1,25 @@
 <template>
   <div ref="footballPitchContainer" class="football-pitch-container" :style="{ backgroundImage: svgImg }">
-    <PlayerCircle class="actor" :number="4" :team-side="TeamSide.OpponentTeam" />
-    <PlayerCircle class="actor" is-keeper :number="1" :team-side="TeamSide.OpponentTeam" />
-    <PlayerCircle class="actor" :number="12" :team-side="TeamSide.MyTeam" />
+    <PlayerCircle
+      v-if="!defenderPlayerPosition.outOfBounds"
+      class="actor"
+      :number="4"
+      :style="{'left': `${defenderPlayerPosition.left}px`, 'top': `${defenderPlayerPosition.top}px`}"
+      :team-side="TeamSide.OpponentTeam"
+    />
+    <PlayerCircle
+      class="actor"
+      is-keeper
+      :number="1"
+      :team-side="TeamSide.OpponentTeam"
+    />
+    <PlayerCircle
+      v-if="!myPlayerPosition.outOfBounds"
+      class="actor"
+      :number="12"
+      :style="{'left': `${myPlayerPosition.left}px`, 'top': `${myPlayerPosition.top}px`}"
+      :team-side="TeamSide.MyTeam"
+    />
   </div>
 </template>
 
@@ -11,6 +28,13 @@
   import { BoardAction, TeamSide } from '@/models/board.action.model.ts'
   import { FootballPitchTemplate } from '@/models/football.pitch.template.model.ts'
   import { FootballPitchVariables } from '@/models/football.pitch.variables.model.ts'
+  import { ValidationException } from '@/models/validation.model.ts'
+
+  interface Position {
+    outOfBounds: boolean
+    left: number
+    top: number
+  }
 
   defineProps({
     boardAction: {
@@ -19,21 +43,59 @@
     },
   })
 
+  const percentageShown = ref(0.55)
+
+  const footballPitchContainer = useTemplateRef('footballPitchContainer')
+
   // data:image/svg+xml;base64,${svgContent}`
   const svgImg = computed(() => {
     const length = ref(105)
     const width = ref(68)
-    const percentageShown = 0.55
+    const ps = percentageShown.value
 
-    const vars = new FootballPitchVariables(length.value, width.value, percentageShown, 2)
+    const vars = new FootballPitchVariables(length.value, width.value, ps, 2)
     const content = new FootballPitchTemplate().apply(vars)
     return `url(data:image/svg+xml;base64,${btoa(content)})`
   })
+
+  const myPlayerPosition = computed(() => {
+    const x = 0.4
+    const y = 0.2
+    return _denormalize(x, y)
+  })
+
+  const defenderPlayerPosition = computed(() => {
+    const x = 1
+    const y = 0.5
+    return _denormalize(x, y)
+  })
+
+  function _denormalize (x: number, y: number): Position {
+    if (!footballPitchContainer.value) {
+      // not ready
+      return { outOfBounds: false, left: 0, top: 0 }
+    }
+
+    if (x < 0 || x > 1) {
+      throw new ValidationException('x is out of bounds')
+    }
+    if (y < 0 || y > 1) {
+      throw new ValidationException('y is out of bounds')
+    }
+
+    if (y > percentageShown.value) {
+      return { outOfBounds: true, left: 0, top: 0 }
+    }
+
+    const width = footballPitchContainer.value.clientWidth * 0.95
+    const height = footballPitchContainer.value.clientHeight * 0.975
+    return { outOfBounds: false, left: width * x, top: (height / 0.55) * y }
+  }
 </script>
 
 <style lang="sass">
 .actor
-  position: relative
+  position: absolute
 .football-pitch-container
   margin: auto
   aspect-ratio: 1.19 // width(with extra space) / length(with extra space) => (68 + 2*2) / ((105+2)*0.55)
@@ -42,7 +104,7 @@
   background-size: 100% 100%
   background-position: center
   background-repeat: no-repeat
-  display: flex
   justify-content: center
   overflow: visible
+  position: relative
 </style>
